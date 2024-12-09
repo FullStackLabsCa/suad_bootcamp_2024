83b629f9ba483ebc580456e7aad961487c2c16b3
surajadhikari98 <rockersooraz80@gmail.com>
2024-12-08 20:10:10 -0500

    added cache mangaer and fix demon assign for each policy

--- pipeline-multithreaded-trade-processing/trading-app/cache-library/src/main/java/io/reactivestax/CacheFactory.java
+++ pipeline-multithreaded-trade-processing/trading-app/cache-library/src/main/java/io/reactivestax/CacheFactory.java
@@ -6,23 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class CacheFactory<K,V> {
 
-    public Cache<K, V> createCacheWithEviction(EvictionPolicy<K, V> policy, long evictionInterval) {
-        Cache<K, V> cache = new Cache<>();
-        Thread evictionThread = new Thread(() -> {
-            while(true){
-                try{
-                    policy.evict(cache);
-                    Thread.sleep(evictionInterval);
-                }catch (InterruptedException e){
-                    Thread.currentThread().interrupt();
-                    log.warn("Eviction thread interrupted");
-                    break;
-                }
-            }
-        });
+    private final EvictionManager<K, V> evictionManager =  new EvictionManager<>();
 
-        evictionThread.setDaemon(true);
-        evictionThread.start();
-        return cache;
+    public void applyEvictionPolicy(Cache<K, V> cache, EvictionPolicy<K, V> policy, long evictionInterval) {
+        evictionManager.registerEvictionPolicy(cache, policy, evictionInterval);
     }
 }
--- pipeline-multithreaded-trade-processing/trading-app/cache-library/src/main/java/io/reactivestax/CachePerformance.java
+++ pipeline-multithreaded-trade-processing/trading-app/cache-library/src/main/java/io/reactivestax/CachePerformance.java
@@ -11,21 +11,27 @@ public class CachePerformance {
 
     public void runFlow() {
         CacheFactory<Integer, String> factory = new CacheFactory<>();
-        Cache<Integer, String> ttlCache = factory.createCacheWithEviction(new TTLEvictionPolicy<>(), 2000);
-        ttlCache.put(1, new CacheEntry<>("physics", 2000));
-        ttlCache.put(2, new CacheEntry<>("chemistry", 4000));
-        ttlCache.put(3, new CacheEntry<>("english", -1));
+        
+        Cache<Integer, String> ttlCache = new Cache<>();
+        ttlCache.put(1, new CacheEntry<>("physics", 5000));
+        ttlCache.put(2, new CacheEntry<>("chemistry", 6000));
+        ttlCache.put(3, new CacheEntry<>("english", 2000));
 
+        Cache<Integer, String> fifoCache = new Cache<>();
+        fifoCache.put(1, new CacheEntry<>("bio", 5000));
+        fifoCache.put(2, new CacheEntry<>("math", 6000));
+        fifoCache.put(3, new CacheEntry<>("engineering", -1));
 
-        Cache<Integer, String> lruCache = factory.createCacheWithEviction(new LRUEvicitionPolicy<>(), 2000);
-        lruCache.put(1, new CacheEntry<>("math", 3000));
-        ttlCache.put(2, new CacheEntry<>("biology", 5000));
-        ttlCache.put(3, new CacheEntry<>("tech-stream", -1));
+        Cache<Integer, String> lruCache = new Cache<>();
+        lruCache.put(1, new CacheEntry<>("java", 5000));
+        lruCache.put(2, new CacheEntry<>("kotlin", 6000));
 
-
-        Cache<Integer, String> fifoCache = factory.createCacheWithEviction(new FIFOEvicitionPolicy<>(), 2000);
-        fifoCache.put(1, new CacheEntry<>("java"));
-        fifoCache.put(2, new CacheEntry<>("kotlin"));
+        factory.applyEvictionPolicy(ttlCache, new TTLEvictionPolicy<>(), 2000);
+        factory.applyEvictionPolicy(fifoCache, new FIFOEvicitionPolicy<>(), 3000);
+        ttlCache.put(4, new CacheEntry<>("updated", 3000));
+        ttlCache.put(2, new CacheEntry<>("updated", 4000));
+        factory.applyEvictionPolicy(lruCache, new LRUEvicitionPolicy<>(), 2000);
+        factory.applyEvictionPolicy(ttlCache, new TTLEvictionPolicy<>(), 2000);
 
     }
 
