8711a58e812a6e6a8bd00903a8baa906103b9bdb
Suraj.Adhikari <suraj.adhikarifullstacklabs@digital>
2024-12-12 14:29:44 -0500

    clean-up

--- pipeline-multithreaded-trade-processing/trading-app/cache-library/src/main/java/io/reactivestax/eviction/LRUEvictionPolicy.java
+++ pipeline-multithreaded-trade-processing/trading-app/cache-library/src/main/java/io/reactivestax/eviction/LRUEvictionPolicy.java
@@ -8,15 +8,15 @@ import java.util.Comparator;
 import java.util.List;
 
 @Slf4j
-public class LRUEvictionPolicy<K, V> implements EvictionPolicy<K,V> {
+public class LRUEvictionPolicy<K, V> implements EvictionPolicy<K, V> {
     @Override
     public void evict(List<Cache<K, V>> cache) {
         cache.forEach(cacheData ->
                 cacheData.getCacheData().entrySet().stream()
-                .min(Comparator.comparing(entry -> entry.getValue().getLastAccessTime()))
-                .ifPresent(entry -> {
-                    cacheData.getCacheData().remove(entry.getKey());
-                    log.info("Evicted (LRU): {}", entry.getKey());
-                }));
+                        .min(Comparator.comparing(entry -> entry.getValue().getLastAccessTime()))
+                        .ifPresent(entry -> {
+                            cacheData.getCacheData().remove(entry.getKey());
+                            log.info("Evicted (LRU): {}", entry.getKey());
+                        }));
     }
 }
