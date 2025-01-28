d0635d2afaf2214c657a86fb0bc9b190ec303372
Suraj.Adhikari <suraj.adhikarifullstacklabs@digital>
2025-01-28 18:14:59 -0500

    added entities

--- active-life-canada/src/main/java/io/reactivestax/active_life_canada/domain/FamilyGroup.java
+++ active-life-canada/src/main/java/io/reactivestax/active_life_canada/domain/FamilyGroup.java
@@ -1,16 +1,28 @@
 package io.reactivestax.active_life_canada.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Data
 @Builder
 public class FamilyGroup {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long familyGroupId;
+    private String familyPin;
+    private Integer credits;
+    private String status;
+    private Integer failedLoginAttempts;
+    @Column(name = "created_ts")
+    private LocalDateTime createdTimeStamps;
+    @Column(name = "last_updated_ts")
+    private LocalDateTime lastUpdatedTimeStamps;
+    private Long createdBy;
+    private Long updatedBy;
+    @OneToMany
+    private FamilyGroup familyGroup;
 }
