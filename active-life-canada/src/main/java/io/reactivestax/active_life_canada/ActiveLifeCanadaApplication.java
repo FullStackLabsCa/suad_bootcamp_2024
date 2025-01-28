1f9e2939808a35742e9a2ebd3e0af47a5bcc090e
Suraj.Adhikari <suraj.adhikarifullstacklabs@digital>
2025-01-28 15:17:11 -0500

    feat: active-life canada added

---
+++ active-life-canada/src/main/java/io/reactivestax/active_life_canada/ActiveLifeCanadaApplication.java
@@ -0,0 +1,13 @@
+package io.reactivestax.active_life_canada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveLifeCanadaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveLifeCanadaApplication.class, args);
+	}
+
+}
