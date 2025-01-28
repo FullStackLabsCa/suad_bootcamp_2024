5442b8b48e387abc3849731c291ea5296f895d14
Suraj.Adhikari <suraj.adhikarifullstacklabs@digital>
2025-01-28 17:45:13 -0500

    fix: parent pom

---
+++ active-life-canada/src/test/java/io/reactivestax/active_life_canada/ActiveLifeCanadaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.reactivestax.active_life_canada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveLifeCanadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
