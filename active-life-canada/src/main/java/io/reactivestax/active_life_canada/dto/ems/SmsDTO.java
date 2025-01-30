992767980b4d614f3d879ae72f41c9cf0d96dce2
Suraj.Adhikari <suraj.adhikarifullstacklabs@digital>
2025-01-30 14:00:09 -0500

    signup and integration with ems-notification service

---
+++ active-life-canada/src/main/java/io/reactivestax/active_life_canada/dto/ems/SmsDTO.java
@@ -0,0 +1,12 @@
+package io.reactivestax.dto;
+
+import lombok.Data;
+
+
+@Data
+public class SmsDTO {
+    private Long id;
+//    private Long clientId;
+    private String phone;
+    private String message;
+}
