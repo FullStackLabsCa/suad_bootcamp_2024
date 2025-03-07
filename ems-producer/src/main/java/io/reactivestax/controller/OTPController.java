2607e4267c0100146b13d7071ca22d1d835807ef
suraj adhikari <surajadhikary40@gmail.com>
2025-03-07 11:12:11 -0500

    OTp controller endpoint appended with the /ems

--- ems-producer/src/main/java/io/reactivestax/controller/OTPController.java
+++ ems-producer/src/main/java/io/reactivestax/controller/OTPController.java
@@ -10,7 +10,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/otp")
+@RequestMapping("/api/v1/ems/otp")
 public class OTPController {
 
     @Autowired
